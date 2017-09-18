d73fd7ef0d51f7f8ef8d9bc9bddf283ae559df53
vladm <vladm@ORIS-15>
2017-09-18 13:55:39 +0300

    Часть 1

---
+++ LogAn.UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    class LogAnalyzerTests
+    {
+        private LogAnalyzer m_analyzer = null;
+
+        [SetUp]
+        public void Setup()
+        {
+            m_analyzer = new LogAnalyzer();
+        }
+
+        [TestCase("filewithgoodextensions.slf", true)]
+        [TestCase("filewithgoodextensions.SLF", true)]
+        [TestCase("filewithbadextensions.foo", false)]
+        public void IsValidLogFileName_VariousExtensions_ReturnsTrue(string file, bool expected)
+        {
+            bool result = m_analyzer.IsValidLogFileName(file);
+
+            Assert.AreEqual(result, expected);
+        }
+
+        [Test]
+        public void IsValidFileName_EmptyFileName_Throws()
+        {
+            var ex = Assert.Catch<Exception>(() => m_analyzer.IsValidLogFileName(""));
+
+            StringAssert.Contains("имя файла должно быть задано", ex.Message);
+        }
+
+        [TestCase("badfile.foo", false)]
+        [TestCase("goodfile.slf", true)]
+        public void IsValidFileName_WhenCalled_ChangesWasLastFileNameValid(string file, bool expected)
+        {
+            LogAnalyzer la = new LogAnalyzer();
+
+            la.IsValidLogFileName(file);
+
+            Assert.AreEqual(expected, la.WasLastFileNameValid);
+        }
+
+    }
+}
---
+++ LogAn/LogAnalyzer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LogAn
+{
+    public class LogAnalyzer
+    {
+        public bool WasLastFileNameValid { get; set; }
+
+        public bool IsValidLogFileName(string fileName)
+        {
+            WasLastFileNameValid = false;
+
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentException("имя файла должно быть задано");
+            }
+
+            if (!fileName.EndsWith(".SLF", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return false;
+            }
+
+            WasLastFileNameValid = true;
+            return true;
+        }
+    }
+}
